64e80073451ac9afa7092280d1e83913c1074a35
salmanmohebi <s.mohebi22@gmail.com>
2018-07-30 13:31:29 +0430

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+import re
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+with open(join(dirname(__file__), 'sudoku', '__init__.py')) as v_file:
+    package_version = re.compile(
+        r'.*__version__=\'(.*?)\'', re.S
+    ).match(v_file.read()).group(1)
+
+dependencies = []
+
+setup(
+    name='farsi_sudoku_solver',
+    version=package_version,
+    author='Salman Mohebi',
+    author_email='s.mohebi22@gmail.com',
+    install_requires=dependencies,
+    packages=find_packages(),
+
+)
