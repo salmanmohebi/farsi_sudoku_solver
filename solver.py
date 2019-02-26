3f1dda941105ddda56b447b1f7da9c1e815ef6b3
sepideh.shams <sepideh.92sh@gmail.com>
2019-02-26 17:50:20 +0330

    finish solver

--- solver.py
+++ solver.py
@@ -16,80 +16,85 @@ def solve_sudoku(num):
             [60, 61, 62, 69, 70, 71, 78, 79, 80]
         ]
     )
+    possible_numbers = find_possible_numbers(numbers, squares)
+    if possible_numbers:
+        possible_numbers = numpy.array(possible_numbers)
+        for i in range(0, 9):
+            counts = numpy.zeros(9)
+            for j in range(0, 9):
+                for k in possible_numbers[squares[i][j]]:
+                    counts[k-1] += 1
+                    if len(possible_numbers[squares[i][j]]) == 1:
+                        numbers[squares[i][j] // 9][squares[i][j] % 9] = possible_numbers[squares[i][j]][0]
+                        possible_numbers[squares[i][j]] = []
+            indices = numpy.where(counts == 1)
+            if len(indices[0]) > 0:
+                for j in range(0, 9):
+                    for k in indices[0]:
+                        if k+1 in possible_numbers[squares[i][j]]:
+                            numbers[squares[i][j]//9][squares[i][j] % 9] = k+1
+                            possible_numbers[squares[i][j]] = []
+        if numpy.alltrue(num == numbers):
+            for i in range(0, 9):
+                counts = numpy.zeros(9)
+                for j in range(0, 9):
+                    for k in possible_numbers[squares[i][j]]:
+                        counts[k - 1] += 1
+                indices = numpy.where(counts == 2)
+                if len(indices[0]) > 0:
+                    for j in range(0, 9):
+                        k = indices[0][0]
+                        if k + 1 in possible_numbers[squares[i][j]]:
+                            numbers[squares[i][j] // 9][squares[i][j] % 9] = k + 1
+                            possible_numbers[squares[i][j]] = []
+                            numbers = solve_sudoku(numbers)
+                            cn = numpy.where(sum(numbers) < 45)
+                            if len(cn[0]) > 0:
+                                numbers = num.copy()
+                            else:
+                                return numbers
+        else:
+            numbers = solve_sudoku(numbers)
+    return numbers
+
+
+def find_possible_numbers(numbers, squares):
     possible_numbers = list()
-    possible_numbers_length_tw = list()
     for i in range(0, 9):
         for j in range(0, 9):
             index = 9 * i + j
             possible_numbers.append([])
-            number_of_square = numpy.where(squares == (9*i+j))
-            possible_numbers[index] = find_possible_numbers(numbers, i, j, squares[number_of_square[0][0]])
-            if len(possible_numbers[index]) == 2:
-                possible_numbers_length_tw.append(index)
-            if numbers[i][j] == 0 and len(possible_numbers[index]) == 0:
-                return False
-
-    possible_numbers = numpy.array(possible_numbers)
-    for i in range(0, 9):
-        counts = numpy.zeros(9)
-        for j in range(0, 9):
-            for k in possible_numbers[squares[i][j]]:
-                counts[k-1] += 1
-            number_of_square = numpy.where(squares == (9 * i + j))
-            p = find_possible_numbers(numbers, i, j, squares[number_of_square[0][0]])
-            if len(p) == 1:
-                numbers[squares[i][j] // 9][squares[i][j] % 9] = p[0]
-                possible_numbers[squares[i][j]] = []
-        indices = numpy.where(counts == 1)
-        if len(indices[0]) > 0:
-            for j in range(0, 9):
-                for k in indices[0]:
-                    if k+1 in possible_numbers[squares[i][j]]:
-                        numbers[squares[i][j]//9][squares[i][j] % 9] = k+1
-                        possible_numbers[squares[i][j]] = []
-    cn = numpy.where(sum(numbers) < 45)
-    if len(cn[0]) > 0:
-        print(numbers)
-        if numpy.alltrue(num == numbers):
-            for e in possible_numbers_length_tw:
-                for f in possible_numbers[e]:
-                    numbers[e // 9][e % 9] = f
-                    possible_numbers[e] = []
-                    solve_sudoku(numbers)
-                    numbers = num.copy()
-        numbers = solve_sudoku(numbers)
-    return numbers
-
-
-def find_possible_numbers(numbers, i, j, squares):
-    possible_numbers = []
-    if numbers[i][j] == 0:
-        rows = numbers[i, numpy.where(numbers[i, ] > 0)]
-        cols = numbers[numpy.where(numbers[:, j] > 0), j]
-        square_number = list()
-        for l in range(9):
-            row = squares[l] // 9
-            col = squares[l] % 9
-            if numbers[row][col] > 0:
-                square_number.append(numbers[row][col])
-        square_number = numpy.array(square_number)
-        x = numpy.array(range(1, 10))
-        t = numpy.unique(numpy.concatenate((rows, cols, square_number), axis=None))
-        possible_numbers = numpy.delete(x, t - 1).tolist()
+            if numbers[i][j] == 0:
+                rows = numbers[i, numpy.where(numbers[i, ] > 0)]
+                cols = numbers[numpy.where(numbers[:, j] > 0), j]
+                number_of_square = numpy.where(squares == (9 * i + j))
+                square = squares[number_of_square[0][0]]
+                square_number = list()
+                for l in range(9):
+                    row = square[l] // 9
+                    col = square[l] % 9
+                    if numbers[row][col] > 0:
+                        square_number.append(numbers[row][col])
+                square_number = numpy.array(square_number)
+                x = numpy.array(range(1, 10))
+                t = numpy.unique(numpy.concatenate((rows, cols, square_number), axis=None))
+                u = numpy.delete(x, t - 1).tolist()
+                possible_numbers[index] = u
+                if len(u) ==0:
+                    return False
     return possible_numbers
 
 
 num = numpy.array([
-    [0, 0, 4, 0, 3, 0, 0, 0, 0],
-    [0, 1, 0, 8, 0, 0, 7, 3, 0],
-    [0, 0, 0, 0, 1, 0, 0, 9, 6],
-    [4, 0, 0, 0, 0, 9, 0, 7, 0],
-    [0, 3, 0, 0, 0, 0, 0, 4, 0],
-    [0, 5, 0, 1, 0, 0, 0, 0, 8],
-    [3, 8, 0, 0, 9, 0, 0, 0, 0],
-    [0, 4, 1, 0, 0, 2, 0, 8, 0],
-    [0, 0, 0, 0, 5, 0, 2, 0, 0]
+    [0, 7, 4, 0, 0, 0, 0, 0, 0],
+    [0, 5, 0, 2, 1, 0, 6, 0, 0],
+    [0, 1, 0, 5, 0, 0, 0, 0, 0],
+    [0, 8, 2, 0, 5, 0, 1, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 4],
+    [0, 0, 6, 0, 7, 0, 2, 5, 0],
+    [0, 0, 0, 0, 0, 6, 0, 1, 0],
+    [0, 0, 7, 0, 2, 8, 0, 9, 0],
+    [0, 0, 0, 0, 0, 0, 7, 3, 0]
 ])
-
 v = solve_sudoku(num)
 print(v)
